3360cf59340fc28d4c318401f35eb37724cbba51
Ilya Konoplev <ilya.konoplev@piano.io>
2025-03-09 15:55:04 +0300

    fix path

--- scripts/model_architecture.py
+++ scripts/model_architecture.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 os.environ['TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD'] = '1'
 
-model = YOLO("best.pt")
+model = YOLO("../base.pt")
 
 for k, v in model.named_parameters():
     print(k)
